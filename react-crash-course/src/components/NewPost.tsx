430d40ff641b01a81446ef49d5281cf6c91c9a19
Mattias <mattias@compare-it.se>
2024-07-10 18:20:02 +0200

    Updated to useState to rerender correct information

--- react-crash-course/src/components/NewPost.tsx
+++ react-crash-course/src/components/NewPost.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 function NewPost() {
+  const [postText, setPostText] = useState("");
   function changeBodyHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    console.log(event.target.value);
+    setPostText(event.target.value);
   }
   return (
     <form className={classes.form}>
@@ -10,6 +12,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{postText}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
