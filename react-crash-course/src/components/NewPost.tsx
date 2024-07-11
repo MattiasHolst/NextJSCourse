d448778a6a58b733c46c67e694525b1df3832cb0
Mattias <mattias@compare-it.se>
2024-07-11 11:28:32 +0200

    Moved states deeper into a component, was not needed further up anymore  - Also added onSubmit function that closes the model and logs the new post to the console

--- react-crash-course/src/components/NewPost.tsx
+++ react-crash-course/src/components/NewPost.tsx
@@ -1,30 +1,45 @@
-import { ChangeEventHandler } from "react";
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 interface NewPostProps {
-  onPostTextChange: ChangeEventHandler<HTMLTextAreaElement> | undefined;
-  onAuthorChange: ChangeEventHandler<HTMLInputElement> | undefined;
-  onCancelPosting: React.MouseEventHandler<HTMLButtonElement>;
+  onCancelPosting: () => void
 }
 
 function NewPost(props: NewPostProps) {
+  const [postText, setPostText] = useState("Default text");
+  const [author, setAuthor] = useState("Mattias");
+
+  function postTextHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setPostText(event.target.value);
+  }
+
+  function authorChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setAuthor(event.target.value);
+  }
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    const postData = {
+      body: postText,
+      author: author,
+    };
+    console.log(postData);
+    props.onCancelPosting();
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleSubmit}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea
-          id="body"
-          required
-          rows={3}
-          onChange={props.onPostTextChange}
-        />
+        <textarea id="body" required rows={3} onChange={postTextHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange} />
+        <input type="text" id="name" required onChange={authorChangeHandler} />
       </p>
       <p className={classes.actions}>
-        <button type='button' onClick={props.onCancelPosting}>Cancel</button>
+        <button type="button" onClick={props.onCancelPosting}>
+          Cancel
+        </button>
         <button>Submit</button>
       </p>
     </form>
--- react-crash-course/src/components/PostsList.tsx
+++ react-crash-course/src/components/PostsList.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
@@ -6,36 +5,24 @@ import Modal from "./Modal";
 
 interface PostListProps {
   isPosting: boolean;
-  onStopPosting: React.MouseEventHandler<HTMLDivElement | HTMLButtonElement>;
+  onStopPosting: () => void
 }
 
 
 function PostsList(props: PostListProps) {
-  const [postText, setPostText] = useState("Default text");
-  const [author, setAuthor] = useState("Mattias");
-
-  function postTextHandler(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    setPostText(event.target.value);
-  }
-
-  function authorChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
-    setAuthor(event.target.value);
-  }
+  
 
   return (
     <>
       {props.isPosting && (
         <Modal onClose={props.onStopPosting}>
           <NewPost
-            onPostTextChange={postTextHandler}
-            onAuthorChange={authorChangeHandler}
             onCancelPosting={props.onStopPosting}
           />
         </Modal>
       )}
 
       <ul className={classes.posts}>
-        <Post author={author} body={postText} />
         <Post author="Gandalf" body="Brotherhood" />
       </ul>
     </>
