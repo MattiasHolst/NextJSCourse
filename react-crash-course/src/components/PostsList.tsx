e963d559066241e8f25940971b7ae5dccdc876b1
Mattias <mattias@compare-it.se>
2024-07-17 12:23:58 +0200

    Added a fake wait timer on backend to be able to create a loading state

--- react-crash-course/src/components/PostsList.tsx
+++ react-crash-course/src/components/PostsList.tsx
@@ -16,12 +16,15 @@ export type PostData = {
 
 function PostsList(props: PostListProps) {
   const [posts, setPosts] = useState<PostData[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -48,17 +51,22 @@ function PostsList(props: PostListProps) {
           />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post, i) => (
             <Post key={i} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
         </div>
       )}
     </>
