5f96e9b1e6249c1946379a032d904e9a625bb47f
Mattias <mattias@compare-it.se>
2024-07-18 11:30:33 +0200

    Refactored and added a loader instead of using own fetching states

--- react-crash-course/src/components/PostsList.tsx
+++ react-crash-course/src/components/PostsList.tsx
@@ -1,6 +1,6 @@
 import Post from "./Post";
 import classes from "./PostsList.module.css";
-import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 export type PostData = {
   body: string;
@@ -8,20 +8,7 @@ export type PostData = {
 };
 
 function PostsList() {
-  const [posts, setPosts] = useState<PostData[]>([]);
-  const [isFetching, setIsFetching] = useState(false);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const resData = await response.json();
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
+  const posts = useLoaderData() as PostData[];
 
   function addPostHandler(postData: PostData) {
     fetch("http://localhost:8080/posts", {
@@ -31,27 +18,22 @@ function PostsList() {
         "Content-Type": "application/json",
       },
     });
-    setPosts((existingPosts: PostData[]) => [postData, ...existingPosts]);
+    //setPosts((existingPosts: PostData[]) => [postData, ...existingPosts]);
   }
 
   return (
     <>
-      {!isFetching && posts.length > 0 && (
+      {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post, i) => (
             <Post key={i} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
-        </div>
-      )}
-      {isFetching && (
-        <div style={{ textAlign: "center", color: "white" }}>
-          <p>Loading posts...</p>
         </div>
       )}
     </>
--- react-crash-course/src/routes/Posts.tsx
+++ react-crash-course/src/routes/Posts.tsx
@@ -1,4 +1,4 @@
-import PostsList from "../components/PostsList";
+import PostsList, { PostData } from "../components/PostsList";
 import { Outlet } from "react-router-dom";
 
 function Posts() {
@@ -13,3 +13,20 @@ function Posts() {
 }
 
 export default Posts;
+
+type PostDataAPI = {
+  posts: [
+    {
+      body: string;
+      author: string;
+    }
+  ];
+};
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/posts");
+  const resData = await response.json().then((res: PostDataAPI) => {
+    return res.posts
+  });
+  return resData;
+}
