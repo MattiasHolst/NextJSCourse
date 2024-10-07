2e2447117d5bc1c737a7f9f9c3fcdf3e5a8be7f8
Mattias Holst <mattias.holst@nexergroup.com>
2024-10-07 10:32:12 +0200

    Added programmatically navigation

--- pages-file-based-routing/pages/clients/[id]/index.tsx
+++ pages-file-based-routing/pages/clients/[id]/index.tsx
@@ -1,7 +1,18 @@
+import { useRouter } from "next/router";
+
 export default function ClientProjectsPage() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    // load data etc...
+
+    router.replace('/clients/mattias/projecta');
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
