66ea2432ec3dfdbb74f30b782cd42add342fc5cc
Mattias Holst <mattias.holst@nexergroup.com>
2024-10-07 14:30:59 +0200

    Added event detail components

--- file-based-event-project/pages/events/[eventId].tsx
+++ file-based-event-project/pages/events/[eventId].tsx
@@ -1,7 +1,28 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
+import { useRouter } from "next/router";
+
 export default function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId as string;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No event found!</p>;
+  }
   return (
-    <div>
-      <h1>The Event Detail Page</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        address={event.location}
+        date={event.date}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 }
