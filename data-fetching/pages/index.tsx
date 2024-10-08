6ad3d517fb8b9af693aa2424ea7398e509426808
Mattias Holst <mattias.holst@nexergroup.com>
2024-10-08 09:37:06 +0200

    Added dummy json file and added getStaticProps function

--- data-fetching/pages/index.tsx
+++ data-fetching/pages/index.tsx
@@ -1,11 +1,31 @@
-function HomePage() {
-    return (
-      <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-      </ul>
-    );
-  }
-  
-  export default HomePage;
+import fs from "fs/promises";
+import path from "path";
+
+function HomePage(props: {
+  products: {
+    id: string;
+    title: string;
+  }[];
+}) {
+  const { products } = props;
+  return (
+    <ul>
+      {/* <li>Produkt 1</li>
+        <li>Produkt 2</li>
+        <li>Produkt 3</li> */}
+
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData.toString());
+  return { props: { products: data.products } };
+}
+
+export default HomePage;
