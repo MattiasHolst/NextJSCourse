b5e3d3cf7d9d034f94a5ced78c9e2ef7237cd271
Mattias Holst <mattias.holst@nexergroup.com>
2024-10-08 10:38:01 +0200

    Added redirect and notFound properties to getStaticProps

--- data-fetching/pages/index.tsx
+++ data-fetching/pages/index.tsx
@@ -1,4 +1,5 @@
 import fs from "fs/promises";
+import { GetStaticProps } from "next";
 import path from "path";
 
 function HomePage(props: {
@@ -17,11 +18,19 @@ function HomePage(props: {
   );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context: GetStaticProps) {
   console.log("(RE-)Generating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData.toString());
+
+  if (!data) {
+    return { redirect: { destination: "/no-data" } };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
   return { props: { products: data.products }, revalidate: 10 };
 }
 
