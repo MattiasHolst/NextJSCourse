accb2c188ff328cfbde8d7efcd31bc36537a8d82
Mattias Holst <mattias.holst@nexergroup.com>
2024-10-09 15:56:42 +0200

    Added a simple form to be able to send data to api

--- api-routes/pages/index.tsx
+++ api-routes/pages/index.tsx
@@ -1,7 +1,29 @@
+import { FormEvent, useRef } from "react";
+
 function HomePage() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const feedbackRef = useRef<HTMLTextAreaElement>(null);
+
+  function submitFormHandler(event: FormEvent) {
+    event.preventDefault();
+
+    const email = emailRef.current?.value;
+    const feedback = feedbackRef.current?.value;
+  }
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows={5} ref={feedbackRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
