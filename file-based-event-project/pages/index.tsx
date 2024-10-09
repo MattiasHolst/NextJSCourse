92e4fefda3fc4b4a7b2f7c66010635f677b3b1de
Mattias Holst <mattias.holst@nexergroup.com>
2024-10-09 14:31:10 +0200

    Added Head (meta data) element

--- file-based-event-project/pages/index.tsx
+++ file-based-event-project/pages/index.tsx
@@ -1,5 +1,6 @@
 import EventList from "@/components/events/event-list";
 import { EventType, getFeaturedEvents } from "@/helpers/api-utils";
+import Head from "next/head";
 
 type FeaturedEventType = {
   featuredEvents: EventType[];
@@ -8,6 +9,13 @@ type FeaturedEventType = {
 export default function HomePage(props: FeaturedEventType) {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great evets that allow you to evolve..."
+        />
+      </Head>
       <EventList events={props.featuredEvents} />
     </div>
   );
