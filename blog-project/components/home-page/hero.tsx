d25e664b3b2e8d5274943409c0a85672f02725e6
Mattias Holst <mattias.holst@nexergroup.com>
2024-10-11 11:19:38 +0200

    Added Hero component and fixed gitignore

--- blog-project/components/home-page/hero.tsx
+++ blog-project/components/home-page/hero.tsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
+
 export default function Hero() {
-  return <p>This is my hero</p>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/mattias.jpg"
+          alt="An image showing Mattias"
+          width={500}
+          height={500}
+        />
+      </div>
+      <h1>Hi, I'm Mattias</h1>
+      <p>
+        I blog about web development - especially front end frameworks like
+        React
+      </p>
+    </section>
+  );
 }
