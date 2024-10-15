bc9bb27335e2dcd82ccfe0d3f458d408ceb61672
Mattias Holst <mattias.holst@nexergroup.com>
2024-10-15 09:41:27 +0200

    Updated imports of syntax-highlighter to reduce page sizes

--- blog-project/components/posts/post-detail/post-content.tsx
+++ blog-project/components/posts/post-detail/post-content.tsx
@@ -1,11 +1,17 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
-
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import ts from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import { PostDataType } from "@/lib/posts-util";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("ts", ts);
+SyntaxHighlighter.registerLanguage("css", css);
 
 function PostContent({ post }: { post: PostDataType }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
